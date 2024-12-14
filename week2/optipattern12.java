b3016a4f194e25806cd1893f3c38bf6859c19ba9
abinaya2105-git <abinaya.s2105@gmail.com>
2024-12-14 14:18:20 +0530

    Add files via upload

---
+++ week2/optipattern12.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String [] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n-1;i++)
+        {
+            System.out.print(" ".repeat(n-i));
+            System.out.print("*".repeat(2*i-1));
+            System.out.println();
+        }
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces in one go
+            System.out.print(" ".repeat(i-1));
+
+            // Print stars in one go
+            System.out.print("*".repeat((2*n)-(2 * i - 1)));
+
+            // Move to the next line
+            System.out.println();
+        }
+        
+    }
+}
